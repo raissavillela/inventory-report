1e0364c3d27c4b5dbc0592e668831ac9bb70803a
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-08-30 15:11:56 -0700

    Requisito 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,33 @@
-def test_product_report() -> None:
-    raise NotImplementedError
+from inventory_report.product import Product
+
+
+def test_product_report():
+    id = "123"
+    product_name = "farinha"
+    company_name = "Farinini"
+    manufacturing_date = "01-05-2021"
+    expiration_date = "02-06-2023"
+    serial_number = "TY68 409C JJ43 ASD1 PL2F"
+    storage_instructions = "precisa ser armazenado em local protegido da luz"
+
+    product = Product(
+        id,
+        product_name,
+        company_name,
+        manufacturing_date,
+        expiration_date,
+        serial_number,
+        storage_instructions,
+    )
+
+    expected_report = (
+        f"The product {id} - {product_name} "
+        f"with serial number {serial_number} "
+        f"manufactured on {manufacturing_date} "
+        f"by the company {company_name} "
+        f"valid until {expiration_date} "
+        "must be stored according to the following instructions: "
+        f"{storage_instructions}."
+    )
+
+    assert str(product) == expected_report
