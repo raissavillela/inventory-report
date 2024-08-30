9316c7294f1f25f1c5963416186da371f7e8ec55
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-08-30 15:34:24 -0700

    Requisitos 3 e 4

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,19 +1,63 @@
-from typing import Dict, Type
+from abc import ABC, abstractmethod
+from typing import List, Dict, Type
+from inventory_report.product import Product
+import json
+import csv
 
 
-class Importer:
-    pass
+class Importer(ABC):
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    @abstractmethod
+    def import_data(self) -> List[Product]:
+        pass
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> List[Product]:
+        try:
+            with open(self.path) as file:
+                data = json.load(file)
+                products = [
+                    Product(
+                        id=item["id"],
+                        product_name=item["product_name"],
+                        company_name=item["company_name"],
+                        manufacturing_date=item["manufacturing_date"],
+                        expiration_date=item["expiration_date"],
+                        serial_number=item["serial_number"],
+                        storage_instructions=item["storage_instructions"],
+                    )
+                    for item in data
+                ]
+                return products
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File {self.path} not found")
 
 
-class CsvImporter:
-    pass
+class CsvImporter(Importer):
+    def import_data(self) -> List[Product]:
+        try:
+            with open(self.path) as file:
+                reader = csv.DictReader(file)
 
+                products = [
+                    Product(
+                        id=row["id"],
+                        product_name=row["product_name"],
+                        company_name=row["company_name"],
+                        manufacturing_date=row["manufacturing_date"],
+                        expiration_date=row["expiration_date"],
+                        serial_number=row["serial_number"],
+                        storage_instructions=row["storage_instructions"],
+                    )
+                    for row in reader
+                ]
+                return products
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File {self.path} not found")
 
-# Não altere a variável abaixo
 
 IMPORTERS: Dict[str, Type[Importer]] = {
     "json": JsonImporter,
