bfb2550b3b37fb4412ea995a920b416f828db06e
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-08-30 15:43:44 -0700

    Requisito 5

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,14 @@
+from typing import List, Optional
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[List[Product]] = None) -> None:
+        self._data = data if data is not None else []
+
+    @property
+    def data(self) -> List[Product]:
+        return self._data.copy()
+
+    def add_data(self, data: List[Product]) -> None:
+        self._data.extend(data)
