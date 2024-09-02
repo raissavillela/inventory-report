45615a178c7e075a4df45134ef88a8e23d803364
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-09-02 08:26:08 -0700

    Requisitos 6 e 7

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -1,2 +1,13 @@
-class Report:
-    pass
+from abc import abstractmethod
+from typing import Protocol
+from inventory_report.inventory import Inventory
+
+
+class Report(Protocol):
+    @abstractmethod
+    def add_inventory(self, inventory: Inventory) -> None:
+        pass
+
+    @abstractmethod
+    def generate(self) -> str:
+        pass
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,40 @@
-class SimpleReport:
-    pass
+from datetime import datetime
+from typing import List
+from inventory_report.inventory import Inventory
+from inventory_report.reports.report import Report
+
+
+class SimpleReport(Report):
+    def __init__(self) -> None:
+        self.inventories: List[Inventory] = []
+
+    def add_inventory(self, inventory: Inventory) -> None:
+        self.inventories.append(inventory)
+
+    def generate(self) -> str:
+        manufacturing_dates = []
+        expiration_dates = []
+        companies = {}
+
+        for inventory in self.inventories:
+            for product in inventory.data:
+                manufacturing_dates.append(product.manufacturing_date)
+                if product.expiration_date > datetime.now().strftime(
+                    "%Y-%m-%d"
+                ):
+                    expiration_dates.append(product.expiration_date)
+                company_name = product.company_name
+                companies[company_name] = companies.get(company_name, 0) + 1
+
+        oldest_manufacturing_date = min(manufacturing_dates)
+        closest_expiration_date = min(expiration_dates)
+
+        largest_inventory = (
+            max(companies, key=companies.get) if companies else None
+        )
+
+        return (
+            f"Oldest manufacturing date: {oldest_manufacturing_date}\n"
+            f"Closest expiration date: {closest_expiration_date}\n"
+            f"Company with the largest inventory: {largest_inventory}"
+        )
